8b8f4762a25b2d5cf89e4f69d1308b7fd5a85689
corey <coreymmartin1991@gmail.com>
2022-06-02 12:17:49 -0500

    initial commit

---
+++ CSSelectionStatements/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
